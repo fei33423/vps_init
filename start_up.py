732e1b0e497dd157eb78b88c2f42cc8e4da58458
Davidyz <hzjlyz@gmail.com>
2018-04-30 16:36:52 +0800

    Detailed description is added.

--- start_up.py
+++ start_up.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""
+A script that launch shadowsocks-libev service (server) automaticly by launching ss-server with all config files in /etc/shadowsocks-libev.
+If using other version of shadowsocks, just modify the path (to the conf files) and command (of launching the service).
+Written for Python2 because py3 is not included in CentOS.
+"""
 import os
 path = '/etc/shadowsocks-libev'
 files = os.listdir(path)

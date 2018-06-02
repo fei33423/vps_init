a761164abc282dd2812d3394a4f6956cbaa7cd48
Davidyz <hzjlyz@gmail.com>
2018-06-02 17:01:53 +0100

    The script for launching ss services is improved so that it can kill existing services and then re-launch them.

--- start_up.py
+++ start_up.py
@@ -5,17 +5,23 @@ If using other version of shadowsocks, just modify the path (to the conf files) 
 Written for Python2 because py3 is not included in CentOS.
 """
 import os
+'''
 # Kill ss services launched by the installation script.
 if 'config' in os.listdir('/root/'):
     os.system('kill {pid}'.format(pid=int(open('/root/config').readline())))
+'''
+list_of_files = ['shadowsocks-libev.pid']
+for i in os.listdir('/etc/shadowsocks-libev/'):
+    list_of_files.append(i.split('.')[0] + '.pid')
 
 # Kill all existing ss services.
-for i in os.listdir('/root/ss_pid/'):
-    os.system('kill {pid}'.format(pid=int(open('/root/ss_pid/{conf}'.format(conf=i)).readline())))
+for i in os.listdir('/var/run/'):
+    if i in list_of_files:
+        os.system('kill {pid}'.format(pid=int(open('/var/run/{conf}'.format(conf=i)).readline())))
 
 # Launch all config files.
 path = '/etc/shadowsocks-libev'
 files = os.listdir(path)
-command = 'nohup ss-server -c /etc/shadowsocks-libev/{conf} -f /root/ss_pid/{pid}'
+command = 'nohup ss-server -c /etc/shadowsocks-libev/{conf} -f /var/run/{pid}.pid'
 for i in files:
     os.system(command.format(conf = i, pid = i.split('.')[0]))

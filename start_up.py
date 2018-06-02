9394f0c6cdb67d30c2fdd822199f64e29cc80a03
Davidyz <hzjlyz@gmail.com>
2018-06-02 23:38:20 +0800

    Try to stop existing ss services before launching services.

--- start_up.py
+++ start_up.py
@@ -5,8 +5,17 @@ If using other version of shadowsocks, just modify the path (to the conf files) 
 Written for Python2 because py3 is not included in CentOS.
 """
 import os
+# Kill ss services launched by the installation script.
+if 'config' in os.listdir('/root/'):
+    os.system('kill {pid}'.format(pid=int(open('/root/config').readline())))
+
+# Kill all existing ss services.
+for i in os.listdir('/root/ss_pid/'):
+    os.system('kill {pid}'.format(pid=int(open('/root/ss_pid/{conf}'.format(conf=i)).readline())))
+
+# Launch all config files.
 path = '/etc/shadowsocks-libev'
 files = os.listdir(path)
-command = 'nohup ss-server -c /etc/shadowsocks-libev/{conf} -f {pid}'
+command = 'nohup ss-server -c /etc/shadowsocks-libev/{conf} -f /root/ss_pid/{pid}'
 for i in files:
     os.system(command.format(conf = i, pid = i.split('.')[0]))

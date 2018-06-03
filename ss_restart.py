ce11efad28a9dace32f95ae06489c59c382b854f
Davidyz <hzjlyz@gmail.com>
2018-06-03 10:07:53 +0800

    Reformation of codes.

--- ss_restart.py
+++ ss_restart.py
@@ -2,15 +2,11 @@
 """
 A script that launch shadowsocks-libev service (server) automaticly by launching ss-server with all config files in /etc/shadowsocks-libev.
 If using other version of shadowsocks, just modify the path (to the conf files) and command (of launching the service).
-Written for Python2 because py3 is not included in CentOS.
+Written for Python2 because py3 is not installed in CentOS by default.
 """
 import os
-'''
-# Kill ss services launched by the installation script.
-if 'config' in os.listdir('/root/'):
-    os.system('kill {pid}'.format(pid=int(open('/root/config').readline())))
-'''
-list_of_files = ['shadowsocks-libev.pid']
+
+list_of_files = ['shadowsocks-libev.pid'] # List of all pid files.
 for i in os.listdir('/etc/shadowsocks-libev/'):
     list_of_files.append(i.split('.')[0] + '.pid')
 
@@ -20,8 +16,8 @@ for i in os.listdir('/var/run/'):
         os.system('kill {pid}'.format(pid=int(open('/var/run/{conf}'.format(conf=i)).readline())))
 
 # Launch all config files.
-path = '/etc/shadowsocks-libev'
-files = os.listdir(path)
-command = 'nohup ss-server -c /etc/shadowsocks-libev/{conf} -f /var/run/{pid}.pid'
+path_of_conf = '/etc/shadowsocks-libev/'
+files = os.listdir(path_of_conf)
+command = 'nohup ss-server -c {conf} -f /var/run/{pid}.pid'
 for i in files:
-    os.system(command.format(conf = i, pid = i.split('.')[0]))
+    os.system(command.format(conf = path_of_conf + i, pid = i.split('.')[0]))
